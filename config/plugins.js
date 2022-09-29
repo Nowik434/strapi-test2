41f85199b9c4e417f732903d2d26b84067cd5b79
Nowik434 <Nowik434@gmail.com>
2022-09-29 08:28:51 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,19 @@ module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
       jwt: {
-        expiresIn: "1m",
+        expiresIn: "5m",
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'marekmarczak@interia.pl',
+        defaultReplyTo: 'marekmarczak@interia.pl',
       },
     },
   },
